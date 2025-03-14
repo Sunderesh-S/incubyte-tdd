c4c8c9d07b1a1066ecce6dccc018fb1ec70c3903
Sunderesh Selvaraj <s4sunderesh@gmail.com>
2025-03-14 13:06:16 +0530

    Handles delimiter of any length and also take care of special chars

--- add.js
+++ add.js
@@ -7,25 +7,36 @@ function Add(input = "") {
     let delimiter = /[\n,]+/;
 
     // Check for a custom delimiter at the beginning of the string
-    const customDelimiterMatch = input.match(/^\/\/(.+)\n/);
-
+    let customDelimiterMatch = input.match(/^\/\/(\[.*\]|\S)\n/);
     if (customDelimiterMatch) {
-        // Use custom delimiter
-        delimiter = new RegExp(`[${customDelimiterMatch[1]}]+`);
+        let customDelimiter = customDelimiterMatch[1];
+
+        if (customDelimiter.startsWith("[") && customDelimiter.endsWith("]")) {
+            // Extract multi-character delimiters inside brackets
+            customDelimiter = customDelimiter.slice(1, -1);
+        }
+
+        // Convert special characters in the delimiter to a valid regex
+        delimiter = new RegExp(customDelimiter.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'), "g");
+
         // Remove delimiter definition from input
         input = input.slice(customDelimiterMatch[0].length);
     }
 
-    input = input.split(delimiter).map(num => Number(num.trim())).filter(num => !isNaN(num));
+    const numbers = input.split(delimiter).map(num => Number(num.trim())).filter(num => !isNaN(num));
 
     // Find negative numbers
-    const negatives = input.filter(num => num < 0);
+    const negatives = numbers.filter(num => num < 0);
     if (negatives.length > 0) {
         throw new Error(`Negatives not allowed: ${negatives.join(", ")}`);
     }
 
-    // Sum up numbers
-    return input.reduce((sum, num) => sum + num, 0);
+    return numbers
+        .filter(num => num <= 1000) // Ignore numbers greater than 1000
+        .reduce((sum, num) => sum + num, 0); // Sum up valid numbers
 }
 
 module.exports = { Add };
+
+
+console.log(Add('//[***]\n1***2***3'))
--- add.test.js
+++ add.test.js
@@ -125,3 +125,43 @@ describe('Add - Negative Numbers', () => {
         expect(() => Add('//;\n-1;\n')).toThrow('Negatives not allowed: -1');
     });
 });
+
+describe('Add - Ignore Numbers Greater Than 1000', () => {
+    test('returns 2 for "2,1001"', () => {
+        expect(Add('2,1001')).toBe(2);
+    });
+
+    test('returns 1000 for "1000,1001"', () => {
+        expect(Add('1000,1001')).toBe(1000);
+    });
+
+    test('returns 0 for "1001,2002"', () => {
+        expect(Add('1001,2002')).toBe(0);
+    });
+
+    test('returns 1 for "1,\n1001" (invalid input)', () => {
+        expect(Add('1,\n1001')).toBe(1);
+    });
+
+    test('returns 3 for "//;\n1;1001;\n2" (invalid input)', () => {
+        expect(Add('//;\n1;1001;\n2')).toBe(3);
+    });
+
+    test('returns 0 for "//;\n1001;\n" (invalid input)', () => {
+        expect(Add('//;\n1001;\n')).toBe(0);
+    });
+});
+
+describe('Add - Delimiters of Any Length', () => {
+    test('returns 6 for "//[***]\n1***2***3"', () => {
+        expect(Add('//[***]\n1***2***3')).toBe(6);
+    });
+
+    test('returns 10 for "//[----]\n1----2----3----4"', () => {
+        expect(Add('//[----]\n1----2----3----4')).toBe(10);
+    });
+
+    test('returns 3 for "//[+]\n1+2"', () => {
+        expect(Add('//[+]\n1+2')).toBe(3);
+    });
+});
